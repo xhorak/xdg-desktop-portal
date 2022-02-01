decc960416c2d834ee63513c1fe8bbce1e8826a5
James Henstridge <james@jamesh.id.au>
2022-02-01 20:12:49 +0800

    webextensions: add a portal for managing WebExtensions native messaging servers.
    This is intended to provide a way for a confined web browser to start
    native code helpers for their extensions. At present it can start the
    servers installed on the host system. But in future this could be
    extended to cover sandboxed native messaging servers too.

--- src/xdg-desktop-portal.c
+++ src/xdg-desktop-portal.c
@@ -58,6 +58,7 @@
 #include "camera.h"
 #include "secret.h"
 #include "wallpaper.h"
+#include "web-extensions.h"
 #include "realtime.h"
 #include "dynamic-launcher.h"
 
@@ -137,6 +138,10 @@ method_needs_request (GDBusMethodInvocation *invocation)
         return TRUE;
       else
         return FALSE;
+    }
+  else if (strcmp (interface, "org.freedesktop.portal.WebExtensions") == 0)
+    {
+      return FALSE;
     }
   else
     {
@@ -246,6 +251,7 @@ on_bus_acquired (GDBusConnection *connection,
   export_portal_implementation (connection, trash_create (connection));
   export_portal_implementation (connection, game_mode_create (connection));
   export_portal_implementation (connection, realtime_create (connection));
+  export_portal_implementation (connection, web_extensions_create (connection));
 
   impls = find_all_portal_implementations ("org.freedesktop.impl.portal.Settings");
   export_portal_implementation (connection, settings_create (connection, impls));
